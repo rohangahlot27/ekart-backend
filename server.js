8ecd0b5bec0d4f6502e186dd8f46535da2aa7f15
rohangahlot27 <rohan.gahlot27@gmail.com>
2025-06-26 13:01:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Load env variables
+dotenv.config();
+
+// Connect to DB
+connectDB();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes (we'll add them soon)
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+const orderRoutes = require('./routes/orderRoutes');
+app.use('/api/orders', orderRoutes);
+
+
+
+app.get('/', (req, res) => {
+  res.send('E-Kart API is running 🛒');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
